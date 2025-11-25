0115379c14d8f1abd29bb09c5661c9dc31adf3d7
bruce1408 <summer56567@163.com>
2025-11-25 18:57:27 +0800

    Refactor demo evaluation script to improve readability and maintainability

---
+++ tools/demo_train_example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 一阶段“跟踪（Track）” 和 “建图（Map）”
+./tools/uniad_dist_train.sh ./projects/configs/stage2_e2e/base_e2e.py 1
+
+
+
+# 二阶段轨迹预测
+# ./tools/uniad_dist_eval.sh \
+# ./projects/configs/stage2_e2e/base_e2e.py \
+# ./ckpts/uniad_base_e2e.pth 1 # (1 is the number of GPUs)
